1ece7743240b4cfddf2eacf8ff2f594945fecc79
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-05-29 22:10:57 +0400

    render roms

--- scripts/prepareRoms.js
+++ scripts/prepareRoms.js
@@ -5,7 +5,9 @@ const sourcePath = path.resolve(__dirname, '../roms');
 const destinationPath = path.resolve(__dirname, '../assets/data/roms.json');
 
 fs.readdir(sourcePath, (_, files) => {
-  const roms = files.filter(file => file.endsWith('.ch8'));
-  const json = JSON.stringify(roms);
+  const roms = files
+    .filter(file => file.endsWith('.ch8'))
+    .map(file => path.parse(file).name);
+    const json = JSON.stringify(roms);
   fs.writeFile(destinationPath, json, 'utf8', () => {});
 });
--- src/index.js
+++ src/index.js
@@ -3,24 +3,32 @@ import { createKeyboardEvent } from './utils.js';
 import roms from '../assets/data/roms.json';
 import './style.css';
 
-let chip8 = null;
+const chip8 = new Chip8();
 
-const startBtn = document.getElementById('start');
-startBtn.addEventListener('click', () => {
-  chip8 = new Chip8();
-  const response = fetch('./roms/octojam1title.ch8')
+function loadRom(romName) {
+  fetch(`./roms/${romName}.ch8`)
     .then(response => response.arrayBuffer())
     .then(buffer => new Uint8Array(buffer))
     .then(rom => {
       chip8.load(rom);
       chip8.start();
     });
-  
-    window.chip8 = chip8;
-  });
+}
 
-function setupRomsShelf() {
-  console.log(roms);
+function renderRoms() {
+  const romsElements = roms.map(rom => {
+    return `<div class='rom' data-key='${rom}'>${rom}</div>`;
+  }).join('');
+
+  const romsContainerElement = document.getElementById('roms');
+  romsContainerElement.insertAdjacentHTML('beforeend', romsElements);
+
+  romsContainerElement.addEventListener('click', (event) => {
+    if (event.target.classList.contains('rom')) {
+      const key = event.target.getAttribute('data-key');
+      loadRom(key);
+    }
+  });
 }
 
 // register listeners for on-screen keyboard
@@ -44,5 +52,5 @@ function setupOnScreenKeyboard() {
   });
 }
 
-setupRomsShelf();
+renderRoms();
 setupOnScreenKeyboard();
