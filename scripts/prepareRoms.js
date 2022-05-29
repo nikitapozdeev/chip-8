a9d3dc7b1b43424af3a63d1b48e82f24c85d552c
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-05-29 21:19:35 +0400

    move assets to dedicated folder, add script for generating roms list

---
+++ scripts/prepareRoms.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = path.resolve(__dirname, '../roms');
+const destinationPath = path.resolve(__dirname, '../assets/data/roms.json');
+
+fs.readdir(sourcePath, (_, files) => {
+  const roms = files.filter(file => file.endsWith('.ch8'));
+  const json = JSON.stringify(roms);
+  fs.writeFile(destinationPath, json, 'utf8', () => {});
+});
